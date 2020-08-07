7843cbe112fe256e67c4666d3d977a7918005289
dai-z <runtime.error@qq.com>
2020-08-07 17:48:36 +0800

    Support serverchan post failure to wechat

--- daka.py
+++ daka.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from urllib.parse import urlencode
 import requests, json, re
 import time, datetime, os, sys
 import random
@@ -115,7 +116,7 @@ class DecodeError(Exception):
     pass
 
 
-def main(username, password, max_delay_sec):
+def main(username, password, max_delay_sec, note_api='', message=''):
     """Hit card process
 
     Arguments:
@@ -152,13 +153,16 @@ def main(username, password, max_delay_sec):
 
     spinner.start(text='正在为您打卡打卡打卡')
     try:
-        # res = dk.post()
+        res = dk.post()
         if str(res['e']) == '0':
             spinner.stop_and_persist(symbol='🦄 '.encode('utf-8'), text='已为您打卡成功！')
         else:
             spinner.stop_and_persist(symbol='🦄 '.encode('utf-8'), text=res['m'])
     except:
         spinner.fail('数据提交失败')
+        if note_api:
+            print('post to serverchan')
+            dk.sess.get(note_api+'?text='+message)
         return 
 
 
@@ -170,6 +174,8 @@ if __name__=="__main__":
         hour = configs["schedule"]["hour"]
         minute = configs["schedule"]["minute"]
         max_delay_sec = configs["schedule"]["max_delay_sec"]
+        note_api = configs["note_api"]
+        message = configs["message"]
     else:
         username = input("👤 浙大统一认证用户名: ")
         password = getpass.getpass('🔑 浙大统一认证密码: ')
@@ -177,17 +183,19 @@ if __name__=="__main__":
         hour = input("\thour: ") or 6
         minute = input("\tminute: ") or 5
         max_delay_sec = input("\tmax delay seconds: ") or 0
+        note_api = ''
+        message = ''
 
     hour = int(hour)
     minute = int(minute)
     max_delay_sec = int(max_delay_sec)
     # Schedule task
     scheduler = BlockingScheduler()
-    scheduler.add_job(main, 'cron', args=[username, password, max_delay_sec], hour=hour, minute=minute)
+    scheduler.add_job(main, 'cron', args=[username, password, max_delay_sec, note_api], hour=hour, minute=minute)
     print('⏰ 已启动定时程序，每天 %02d:%02d ~ %02d:%02d为您打卡' %(int(hour), int(minute), int(hour+max_delay_sec//3600), int(minute+(max_delay_sec%3600)//60)))
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
     # Test
-    # main(username, password, 0)
+    # main(username, password, 0, note_api, message)
 
     try:
         scheduler.start()
